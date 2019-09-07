2e0da8cc1472f3eeb1d70aece2c3565ec43d2a61
brian <bchou515@gmail.com>
2019-09-07 19:02:19 -0400

    loaded ethers

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import './App.css';
 import { EthersContext } from './EthersContext.js';
+import { ethers } from 'ethers';
 
 function App() {
-  return (
-    <div>
-        
-    </div>
-  );
+    const [ethersContext, setEthersContext] = useContext(EthersContext);
+
+    useEffect(() => {
+        if (typeof window.ethereum !== 'undefined'
+            || (typeof window.web3 !== 'undefined')) {
+            // console.log(window.web3.version);
+            // Web3 browser user detected. You can now use the provider.
+            let provider = window['ethereum'] || window.web3.currentProvider
+            //NOTE: must wrap window.etherm to get provider, not window.web3
+            provider = new ethers.providers.Web3Provider(window.ethereum);
+            // console.log(await provider.ready);
+            setEthersContext(state => ({ ...ethersContext, provider }));
+        }
+    }, []);
+
+    return (
+        <div>
+            
+        </div>
+    );
 }
 
 export default App;
