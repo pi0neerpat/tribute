6bf1a0efec57e04100d393acea84662a618cbf06
brian <bchou515@gmail.com>
2019-09-07 19:25:34 -0400

    get ethers context working

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,28 +1,34 @@
 import React, { useState, useEffect, useContext } from 'react';
 import './App.css';
-import { EthersContext } from './EthersContext.js';
+import { EthersContext, EthersContextProvider } from './EthersContext.js';
 import { ethers } from 'ethers';
+import rDAIContract from './contracts/rDAI.abi.json'
 
 function App() {
     const [ethersContext, setEthersContext] = useContext(EthersContext);
 
-    useEffect(() => {
-        if (typeof window.ethereum !== 'undefined'
+    if (typeof window.ethereum !== 'undefined'
             || (typeof window.web3 !== 'undefined')) {
+            
             // console.log(window.web3.version);
             // Web3 browser user detected. You can now use the provider.
             let provider = window['ethereum'] || window.web3.currentProvider
             //NOTE: must wrap window.etherm to get provider, not window.web3
             provider = new ethers.providers.Web3Provider(window.ethereum);
-            // console.log(await provider.ready);
+            //let contract = new ethers.Contract('', rDAIContract, provider);
+
             setEthersContext(state => ({ ...ethersContext, provider }));
-        }
-    }, []);
+            console.log(ethersContext);
+    }
+
+    console.log(ethersContext);
 
     return (
-        <div>
-            
-        </div>
+        <EthersContextProvider>
+            <div>
+                
+            </div>
+        </EthersContextProvider>
     );
 }
 
