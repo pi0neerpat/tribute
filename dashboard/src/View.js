2b788bbd83a2cac4fe42ea0eb97c93462e1125d1
Ethan Wessel <ejwessel@gmail.com>
2019-09-07 20:48:42 -0400

    adjustment and loading contract abi

--- dashboard/src/View.js
+++ dashboard/src/View.js
@@ -6,6 +6,7 @@ import rDAIContract from './contracts/rDAI.abi.json'
 function View() {
   const [ethersContext, setEthersContext] = useContext(EthersContext);
   const [selectedAddress, setSelectedAddress] = useState();
+  const rDAIAddress = "0xeA718E4602125407fAfcb721b7D760aD9652dfe7";
 
   // Detect when account changes
   window.ethereum.on('accountsChanged', function (accounts) {
@@ -23,12 +24,14 @@ function View() {
       let provider = window['ethereum'] || window.web3.currentProvider
       //NOTE: must wrap window.etherm to get provider, not window.web3
       provider = new ethers.providers.Web3Provider(window.ethereum);
-      let contract = new ethers.Contract('', rDAIContract, provider);
+      let contract = new ethers.Contract(rDAIAddress, rDAIContract, provider);
       if (ethersContext.contract === undefined) {
         setEthersContext(state => ({ ...ethersContext, contract }));
       }
     }
   }, [ethersContext]);
+
+  console.log(ethersContext.contract);
 
   return (
     <div>
