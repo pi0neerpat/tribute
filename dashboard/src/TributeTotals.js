eff60bb7b924c071f57acfbeccb218adc528fcfa
Ethan Wessel <ejwessel@gmail.com>
2019-09-08 00:31:04 -0400

    TT

---
+++ dashboard/src/TributeTotals.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default function TributeTotals({ principal, unallocated }) {
+  return(
+    <div>
+      Tribute
+      Principal
+      Unallocated Tribute
+    </div>
+  );
+}
